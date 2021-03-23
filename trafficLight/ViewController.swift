e52d09270f8c10f529eb80c994e1b27777b9876c
ErrorV9 <vlad.nikitenkov95@gmail.com>
2021-03-23 22:34:38 +0300

    add color light

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var orangeLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
