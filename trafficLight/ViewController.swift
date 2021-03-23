6191928227680413f7e659d380660fd2bfaf5f80
ErrorV9 <vlad.nikitenkov95@gmail.com>
2021-03-23 22:11:31 +0300

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by ErrorV9 on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
