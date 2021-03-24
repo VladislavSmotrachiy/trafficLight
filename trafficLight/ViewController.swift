4470d2ad3c0272cda0ed3d54450edc8383a53461
ErrorV9 <vlad.nikitenkov95@gmail.com>
2021-03-24 12:35:55 +0300

    switching color

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,33 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var redLight: UIView!
-    @IBOutlet weak var greenLight: UIView!
-    @IBOutlet weak var orangeLight: UIView!
     
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var redLignt: UIView!
+    @IBOutlet weak var orangeLight: UIView!
     @IBOutlet weak var button: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         button.layer.cornerRadius = 10
-        viewWillLayoutSubviews()
-        
-    
-       
-        
-        
+        redLignt.alpha = 0.3
+        orangeLight.alpha = 0.3
+        greenLight.alpha = 0.3
+
     }
     
+    enum AllLight {
+        case redLignt, orangeLight, greenLight
+    }
+    var allLigth = AllLight.redLignt
+    
+        
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        redLight.layer.cornerRadius = redLight.frame.height / 2.0
-        greenLight.layer.cornerRadius = redLight.frame.height / 2.0
-        orangeLight.layer.cornerRadius = redLight.frame.height / 2.0
+        redLignt.layer.cornerRadius = redLignt.frame.height / 2.0
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2.0
+        orangeLight.layer.cornerRadius = orangeLight.frame.height / 2.0
         }
     
-    @IBAction func switchTrafficLight() {
-        
-    }
     
-}
+    
+    @IBAction func switchTrafficLight() {
+        switch allLigth {
+        case .redLignt:
+            greenLight.alpha = 0.3
+            redLignt.alpha = 1
+            allLigth = .orangeLight
+            break
+        case .orangeLight:
+            redLignt.alpha = 0.3
+            orangeLight.alpha = 1
+            allLigth = .greenLight
+        case .greenLight:
+            orangeLight.alpha = 0.3
+            greenLight.alpha = 1
+            allLigth = .redLignt
+        }
+        }
+        
+            
+
+        
+    
+        
+       
+            
+        
+        }
+    
+    
+
+    
+
 
